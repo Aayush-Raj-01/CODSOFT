c516104a7d5b4c3aa89d5b4d299fd459be2cb3c6
Aayush Raj <aush110804@gmail.com>
2026-02-16 00:32:50 +0530

    Started Simple Calculator

---
+++ Tasl - 2 Simple Calculator/Simple_calculator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+using namespace std;
+int main(){
+    cout >> "WELCOME TO A SIMPLE CALCULATOR\n";
+    cout >> "This Calculator only supports basic operations like = (+,-,*,/)\n";
+    cou
+}
