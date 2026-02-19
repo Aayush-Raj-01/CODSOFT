447df4be55eeba36ffdf69500551a48456d5e6b3
Aayush Raj <aush110804@gmail.com>
2026-02-20 00:30:39 +0530

    update

--- Tasl - 2 Simple Calculator/Simple_calculator.cpp
+++ Tasl - 2 Simple Calculator/Simple_calculator.cpp
@@ -3,7 +3,30 @@
 
 using namespace std;
 int main(){
-    cout >> "WELCOME TO A SIMPLE CALCULATOR\n";
-    cout >> "This Calculator only supports basic operations like = (+,-,*,/)\n";
-    cou
+    string eq,temp;
+    int ans=0,temp1=1;
+    cout << "WELCOME TO A SIMPLE CALCULATOR\n";
+    cout << "This Calculator only supports basic operations like = (+,-,*,/)\n";
+    cout << "YOUR equation : ";
+    getline(cin,eq);
+    for(int i =0 ; i < eq.size() ; i++){
+        if(isdigit(eq[i])){
+            temp += eq[i];
+        }
+        else if(eq[i]=='+'){
+            ans += stoi(temp);
+        }
+        else if(eq[i]=='-'){
+            ans = stoi(temp);
+        }
+        else if(eq[i]=='*'){
+            if(!temp1) temp1++;
+            ans *= stoi(temp);
+        }
+        else if(eq[i]=='/'){
+            ans /= stoi(temp);
+        }
+    }
+    cout << "Your Answer is = " << ans;
+    return 0 ;
 }
